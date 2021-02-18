63450ce7558335189d81b85197aef78b865092dc
Haz-15 <Ab2388@njit.edu>
2021-02-18 12:03:16 -0500

    Final Udderz application] Final egg timer

--- Egg_Time/ViewController.swift
+++ Egg_Time/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var labelOutput: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
+    var eggTimes = ["Soft" : 3, "Medium" : 5, "Hard" : 15]
+    
+    
+    var timer = Timer()
+    var startTime = 0
+    var totalTime = 0
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        startTime = 0
+        
+        progressView.progress = 0.0
+        timer.invalidate()
+        var hardness = sender.currentTitle!
+        
+        
+        totalTime = eggTimes[hardness]!
+        
+        labelOutput.text = hardness
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:
+                                #selector(updateCounter), userInfo: nil, repeats: true)
     }
-
-
+    @objc func updateCounter() {
+        //example functionality
+        if startTime < totalTime {
+        
+           
+            startTime += 1
+            progressView.progress = Float(startTime)/Float(totalTime)
+        
+        }
+        else {
+            labelOutput.text = "Done!"
+            timer.invalidate()
+            //add alarm for more
+        }
+    }
+    
+    
+    
 }
 
