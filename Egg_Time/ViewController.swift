bc71350e63532e58af6355fcce84d916f5ce4fea
Haz-15 <Ab2388@njit.edu>
2021-04-01 11:28:55 -0400

    Final

--- Egg_Time/ViewController.swift
+++ Egg_Time/ViewController.swift
@@ -13,37 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressView: UIProgressView!
     
     
-    var eggTimes = ["Soft" : 3, "Medium" : 5, "Hard" : 15]
-    
-    
+    let eggTimes = ["Soft" : 3, "Medium" : 5, "Hard" : 15]
     var timer = Timer()
-    var startTime = 0
+    var secondsPassed = 0
     var totalTime = 0
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         
-        startTime = 0
-        
-        progressView.progress = 0.0
         timer.invalidate()
-        var hardness = sender.currentTitle!
-        
-        
+        let hardness = sender.currentTitle!
         totalTime = eggTimes[hardness]!
         
+        progressView.progress = 0.0
+        secondsPassed = 0
+
         labelOutput.text = hardness
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:
+       timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:
                                 #selector(updateCounter), userInfo: nil, repeats: true)
     }
     @objc func updateCounter() {
         //example functionality
-        if startTime < totalTime {
-        
-           
-            startTime += 1
-            progressView.progress = Float(startTime)/Float(totalTime)
-        
+        if secondsPassed < totalTime {
+            secondsPassed +=  1
+            progressView.progress = Float(secondsPassed)/Float(totalTime)
+            
         }
         else {
             labelOutput.text = "Done!"
